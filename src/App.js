324ac0bcc37c54202a88d6d0660053ff2b7255d7
43215-ekaleidoAnuj <anuj.kumar@ekaleido.co>
2021-09-12 18:42:23 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavbarText,
   Container,
   Row,
   Col,
@@ -25,12 +24,12 @@ import {
   FaUserAlt,
   FaWhatsapp,
   FaCloud,
-  FaLock,
   FaPhoneAlt,
   FaFacebookSquare,
   FaLinkedin,
   FaYoutube,
   FaInstagram,
+  FaBrain,
 } from "react-icons/fa";
 import { ImLocation2 } from "react-icons/im";
 import { MdEmail } from "react-icons/md";
@@ -47,26 +46,37 @@ function App() {
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="mr-auto" navbar></Nav>
-            <NavbarText>"Monthly fees collection huva easy"</NavbarText>
           </Collapse>
         </Container>
       </Navbar>
       <Container>
         <Row>
-          <Col lg="8" sm="8" style={{ display: "flex", alignItems: "center" }}>
-            <img src={logos} alt="logo" width="100%" height="auto" />
+          <Col
+            lg="8"
+            sm="8"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <img src={logos} alt="logo" width="auto" height="50%" />
+            <h2>
+              "Monthly fees
+              <br /> collection <br /> hua easy"
+            </h2>
           </Col>
           <Col lg="4" sm="4" style={{ display: "flex", alignItems: "center" }}>
             <img src={OKLG7C0} alt="logo" width="100%" height="auto" />
           </Col>
         </Row>
       </Container>
-      <div className="about" style={{ background: "#d3c60d" }}>
+      <div className="about" style={{ background: "#043d9e" }}>
         <Row>
-          <h2>ABOUT THE COMPANY</h2>
-          <h4>Key features of our company</h4>
+          <h2 style={{ color: "#fff" }}>ABOUT THE COMPANY</h2>
+          <h4 style={{ color: "#fff" }}>Key features of our company</h4>
           <Col lg="3" sm="12">
-            <Card>
+            <Card className="about-card">
               <FaUserAlt size={"3em"} />
               <CardBody>
                 <CardTitle tag="h5">User Management</CardTitle>
@@ -74,46 +84,47 @@ function App() {
                   Businesses can create profile and can add subscription of all
                   its users from a single device. Collecting via cash or cheque?
                   Use our app to keep a track of it . Its all in one platform
-                  for all your user management .
+                  for all your user management
                 </CardText>
               </CardBody>
             </Card>
           </Col>
           <Col lg="3" sm="12">
-            <Card>
+            <Card className="about-card">
               <FaWhatsapp size={"3em"} />
               <CardBody>
                 <CardTitle tag="h5">Send payment reminders</CardTitle>
                 <CardText>
                   Send timely reminders with payment link to all your due
                   customers via whatsapp and receive money directly in your
-                  account..
+                  account
                 </CardText>
               </CardBody>
             </Card>
           </Col>
           <Col lg="3" sm="12">
-            <Card>
+            <Card className="about-card">
               <FaCloud size={"3em"} />
               <CardBody>
                 <CardTitle tag="h5">Analytics</CardTitle>
                 <CardText>
                   Real time data of all your users paid , unpaid, payment
                   received, payment pending and many more to showcase your
-                  business performance.
+                  business performance
                 </CardText>
               </CardBody>
             </Card>
           </Col>
           <Col lg="3" sm="12">
-            <Card>
-              <FaLock size={"3em"} />
+            <Card className="about-card">
+              <FaBrain size={"3em"} />
               <CardBody>
-                <CardTitle tag="h5">Secure and safe</CardTitle>
+                <CardTitle tag="h5">Smart Communication</CardTitle>
                 <CardText>
-                  Real time data of all your users paid , unpaid, payment
-                  received, payment pending and many more to showcase your
-                  business performance.
+                  Our algorithm makes contact decisions by measuring the effect
+                  of messages (opens, clicks, responses, etc.). If mail bounces,
+                  an SMS is automatically sent, if you don't click on the
+                  payment link, a WhatsApp is sent
                 </CardText>
               </CardBody>
             </Card>
@@ -121,62 +132,65 @@ function App() {
         </Row>
       </div>
       <Container>
-        <Row>
-          <h2>THE TEAM</h2>
-          <h4>
-            "We are a small team based out in lucknow,and passionate about
-            building product for small businesses"
-          </h4>
-          <Col lg="4" sm="12">
-            <Card>
-              <CardImg top width="100%" src={shubham} alt="shubham" />
-              <CardBody>
-                <CardTitle tag="h5">User Management</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                  Shubham Yadav
-                </CardSubtitle>
-                <CardText>
-                  MBA-Symbiosis Pune <br /> 3yr work ex working for startups{" "}
-                  <br /> shubh28107@gmail.com
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col lg="4" sm="12">
-            <Card>
-              <CardImg top width="100%" src={bhoop} alt="bhoop" />
-              <CardBody>
-                <CardTitle tag="h5">Bhoop Narayan Singh</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                  Co-Founder & CPO
-                </CardSubtitle>
-                <CardText>
-                  B.Tech-IIT Dhanbad <br /> 1yr work ex in L&T <br />{" "}
-                  bhoopns16@gmail.com
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col lg="4" sm="12">
-            <Card>
-              <CardImg top width="100%" src={abhi} alt="bhoop" />
-              <CardBody>
-                <CardTitle tag="h5">Abhinandan Singh</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                  Co-founder & CTO
-                </CardSubtitle>
-                <CardText>
-                  B.Tech-AIT Pune(final year) <br />
-                  Full stack developer
-                  <br />
-                  abhinandans00778@gmail.com
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+        <div style={{ margin: "3rem 0" }}>
+          <Row>
+            <h2>THE TEAM</h2>
+            <h4>
+              "We are a small team based out in lucknow,and passionate about
+              building product for small businesses"
+            </h4>
+            <Col lg="4" sm="12">
+              <Card>
+                <CardImg top width="100%" src={shubham} alt="shubham" />
+                <CardBody>
+                  <CardTitle tag="h5">Shubham Yadav</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Co-Founder & CEO
+                  </CardSubtitle>
+                  <CardText>
+                    MBA-Symbiosis Pune <br /> 3yr work ex working for startups{" "}
+                    <br />
+                    <MdEmail /> shubh28107@gmail.com
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="4" sm="12">
+              <Card>
+                <CardImg top width="100%" src={bhoop} alt="bhoop" />
+                <CardBody>
+                  <CardTitle tag="h5">Bhoop Narayan Singh</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Co-Founder & CPO
+                  </CardSubtitle>
+                  <CardText>
+                    B.Tech-IIT Dhanbad <br /> 1yr work ex in L&T <br />{" "}
+                    <MdEmail /> bhoopns16@gmail.com
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="4" sm="12">
+              <Card>
+                <CardImg top width="100%" src={abhi} alt="bhoop" />
+                <CardBody>
+                  <CardTitle tag="h5">Abhinandan Singh</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Co-founder & CTO
+                  </CardSubtitle>
+                  <CardText>
+                    B.Tech-AIT Pune(final year) <br />
+                    Full stack developer
+                    <br />
+                    <MdEmail /> abhinandans00778@gmail.com
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </Container>
-      <Row className="stats">
+      <Row className="stats" style={{ color: "#fff" }}>
         <Col lg="6" className="stats-item">
           <h2>5</h2>
           <p>Partners</p>
@@ -223,19 +237,20 @@ function App() {
           </table>
         </Col>
       </Container>
-      <div className="pricing">
+      <div className="pricing" style={{ color: "#fff" }}>
         <Row>
           <h2>PRICING</h2>
           <h1>"Enjoy the first month for free".</h1>
           <br />
           <br />
-          <p className="price">2999/- for 6 months | 4999/- for 12 months</p>
+          <p className="price">2999/- for 6 months </p>
+          <p className="price"> 4999/- for 12 months</p>
         </Row>
       </div>
       <div className="contact">
         <Row>
           <h2>CONTACT</h2>
-          <h1>Lets get in touch. Send us a message:</h1>
+          <h4>Lets get in touch. Send us a message:</h4>
           <Col lg="12" sm="12">
             <div className="contact-item">
               <ImLocation2 /> LUCKNOW, India
@@ -252,42 +267,46 @@ function App() {
 
       <div className="social-media">
         <div className="social-item">
-          <FaFacebookSquare color={'#fff'}/>
           <a
+            style={{ textDecoration: "none", color: "#fff" }}
             href="https://m.facebook.com/BillPay-102195622210582/"
             target="_blank"
             rel="noreferrer"
           >
+            <FaFacebookSquare size={"3em"} color={"#fff"} />
             <p>Facebook</p>
           </a>
         </div>
         <div className="social-item">
-          <FaInstagram color={'#fff'}/>
           <a
+            style={{ textDecoration: "none", color: "#fff" }}
             href="https://www.instagram.com/billpaytech/"
             target="_blank"
             rel="noreferrer"
           >
+            <FaInstagram size={"3em"} color={"#fff"} />
             <p>Instagram</p>
           </a>
         </div>
         <div className="social-item">
-          <FaYoutube color={'#fff'}/>
           <a
+            style={{ textDecoration: "none", color: "#fff" }}
             href="https://www.youtube.com/channel/UCzi1XJ4c9niNjieqCUA3JDg/videos"
             target="_blank"
             rel="noreferrer"
           >
+            <FaYoutube size={"3em"} color={"#fff"} />
             <p>Youtube</p>
           </a>
         </div>
         <div className="social-item">
-          <FaLinkedin color={'#fff'} />
           <a
+            style={{ textDecoration: "none", color: "#fff" }}
             href="https://www.linkedin.com/company/billpaytech/"
             target="_blank"
             rel="noreferrer"
           >
+            <FaLinkedin size={"3em"} color={"#fff"} />
             <p>LinkedIn</p>
           </a>
         </div>
